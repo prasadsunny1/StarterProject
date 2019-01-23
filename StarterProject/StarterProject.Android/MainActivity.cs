e536e2f68b3728654d6ef0df078ff3e0f6040f62
sanni prasad <prasadsunny1@gmail.com>
2019-01-24 00:28:44 +0530

    Initial Commit

---
+++ StarterProject/StarterProject.Android/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Acr.UserDialogs;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Android.Runtime;
+
+namespace StarterProject.Droid
+{
+    [Activity(Label = "StarterProject", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            UserDialogs.Init(() => (Activity)Forms.Context);
+            Xamarin.Essentials.Platform.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+       
+    
+    
+}
+
