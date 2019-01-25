a377a47d07fddbfe14801c06772bcd053d4946c8
sanni prasad <prasadsunny1@gmail.com>
2019-01-25 20:00:30 +0530

    Added Retry using poly

--- StarterProject/StarterProject/Services/MovieRepository.cs
+++ StarterProject/StarterProject/Services/MovieRepository.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using MonkeyCache.LiteDB;
+using Polly;
 using StarterProject.Interfaces;
 using Xamarin.Essentials;
 
@@ -18,21 +22,33 @@ namespace StarterProject.Services
         public async Task<string> GetMovie(string title)
         {
             var key = $"Movies_{title}";
-            
+
 //            disconnected and cache available
             if (Connectivity.NetworkAccess != NetworkAccess.Internet && Barrel.Current.Exists(key))
             {
                 return Barrel.Current.Get<string>(key);
             }
+
 //            connected and cache available
             if (!Barrel.Current.IsExpired(key) && Barrel.Current.Exists(key))
             {
                 return Barrel.Current.Get<string>(key);
-            }        
-            
-            var movies = await _omdbApi.GetMoviesByTitle(title);
-            Barrel.Current.Add(key,movies,TimeSpan.FromSeconds(10));
-            
+            }
+
+            var movies = await Policy
+                .Handle<WebException>()
+                .Or<HttpRequestException>()
+                .Or<TimeoutException>()
+                .WaitAndRetryAsync
+                (
+                    retryCount: 3,
+                    sleepDurationProvider: retryAttempt =>
+                    {
+                        Debug.WriteLine($"Attempt {retryAttempt}");
+                        return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt));
+                    }).ExecuteAsync(async () => await _omdbApi.GetMoviesByTitle(title));
+
+            Barrel.Current.Add(key, movies, TimeSpan.FromSeconds(10));
             return movies;
         }
     }
